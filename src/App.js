abd7c6e4a0a745ced9f6488b45a3763b34a6271f
florinv88 <florinvisan022@gmail.com>
2022-11-06 13:22:22 +0200

    Initial deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="card">
+        <img src={require('./images/qr.png')} alt="" />
+        <h3>Improve your front-end skills by building projects</h3>
+        <h4>Scan the QR code to visit Frontend Mentor and take your coding skills to next level</h4>
+      </div>
     </div>
   );
 }
